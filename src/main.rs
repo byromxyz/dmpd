fcf0b56e76929d874101b07feec62b204da01440
Ash Byrom <ashley.byrom@dazn.com>
2024-12-21 08:48:26 +0000

    task: self update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use std::{fs, path::PathBuf};
+use std::{
+    env,
+    fs::{self, File},
+    io::{self, Read},
+    os::unix::fs::PermissionsExt,
+    path::PathBuf,
+};
 
 use ::serde::Deserialize;
 use clap::Parser;
@@ -113,6 +119,21 @@ fn compare_versions(current_version: &str, provided_version: &str) -> Result<i8,
 }
 
 fn check_updates() {
+    // Check if running in dev mode
+    if cfg!(debug_assertions) {
+        println!("Running in debug mode. Skipping updates.");
+        return;
+    }
+
+    // Get the current system architecture and OS
+    let target_arch = env::consts::ARCH; // e.g., "x86_64", "aarch64"
+    let target_os = env::consts::OS; // e.g., "macos", "linux", "windows"
+
+    println!(
+        "Checking for updates:\n  Target arch: {}\n  Target OS: {}",
+        target_arch, target_os
+    );
+
     let client = Client::new();
 
     let http_result = client
@@ -134,17 +155,87 @@ fn check_updates() {
 
     let version = &body.tag_name;
 
-    // Current version of the binary (from Cargo.toml)
     let current_version = env!("CARGO_PKG_VERSION");
 
+    println!(
+        "  Current version: {}\n  Latest version: {}",
+        current_version, version
+    );
+
+    let latest_asset = &body
+        .assets
+        .iter()
+        .find(|asset| asset.name.contains(target_arch))
+        .expect(&format!(
+            "Unable to match architecture {} to a download URL.",
+            target_arch
+        ));
+
     if compare_versions(current_version, version) >= Ok(0) {
+        println!("Current version is up to date");
+
         return;
     }
 
+    println!("Newer version available");
+
+    let current_exe = env::current_exe().expect("Could not get current exe");
+    let backup_path = current_exe.with_extension("bak");
+
+    let new_binary_path = env::current_dir()
+        .expect("Could not get current exe directory")
+        .join("new_binary");
+
     println!(
-        "A newer version {} is available.\nSee {}",
-        version, &body.html_url
+        "Attempting to update.\n  Current path:  {:?}\n  Backup path:  {:?}\n  Tmp path:  {:?}",
+        current_exe, backup_path, new_binary_path
     );
+
+    let response = reqwest::blocking::get(&latest_asset.browser_download_url)
+        .expect("Could not block download the updated binary");
+
+    let mut file =
+        File::create(&new_binary_path).expect("Could not create new file for downloaded binary");
+
+    let content_length = response.content_length().unwrap_or(0);
+
+    println!("Content-Length: {} MB", content_length / 1_000_000);
+
+    println!("Downloaded new binary ({} bytes)", content_length);
+
+    io::copy(&mut response.take(content_length), &mut file)
+        .expect("Unable to copy downloaded binary to file location");
+
+    println!("Copied binary to tmp location");
+
+    let mut permissions = fs::metadata(&new_binary_path)
+        .expect("Could not get metadata for downloaded binary")
+        .permissions();
+
+    permissions.set_mode(0o755); // Owner: read, write, execute; Group: read, execute; Others: read, execute
+    fs::set_permissions(&new_binary_path, permissions).expect("Could not make exe executable");
+
+    println!("New binary is executable");
+
+    // let response = reqwest::blocking::get(url)?;
+
+    // Rename current executable to backup
+    fs::rename(&current_exe, &backup_path).expect("Unable to rename current exe as backup");
+
+    println!("Current binary renamed as backup");
+
+    // Rename new binary to current executable's name
+    fs::rename(new_binary_path, &current_exe).expect("Unable to rename new exe as current");
+
+    println!("New binary renamed as current");
+
+    // TODO: (??) On Windows, schedule deletion of the backup after a delay
+    // #[cfg(target_os = "windows")]
+    // {
+    //     Command::new("cmd")
+    //         .args(&["/C", "ping", "127.0.0.1", "-n", "5", ">", "nul", "&", "del", "/Q", backup_path.to_str().unwrap()])
+    //         .spawn()?;
+    // }
 }
 
 fn main() {
